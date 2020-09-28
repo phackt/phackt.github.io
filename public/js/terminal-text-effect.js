2debb9825034ea2472e67eb1c170789a2109879c
phackt <phackt@protonmail.com>
2020-09-28 16:33:15 +0200

    minor change js terminal effect

--- public/js/terminal-text-effect.js
+++ public/js/terminal-text-effect.js
@@ -1,6 +1,6 @@
 // Credits to https://codepen.io/Tbgse/full/dYaJyJ
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Web Security', 'Pentesting', 'Development', 'Labs & Certifications'], 'text',['#d9230f']);
+ consoleText(['Web Security', 'Pentesting', 'Development', 'Labs & Certifications', 'AD Exploitation'], 'text',['#d9230f']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
